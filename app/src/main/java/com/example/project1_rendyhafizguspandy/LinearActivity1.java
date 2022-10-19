559c342169fd2058be8757cec62a494bccb5fe86
RendyHafizt <rendy.hafizt@gmail.com>
2022-10-19 11:56:46 +0700

    pratikum2

---
+++ app/src/main/java/com/example/project1_rendyhafizguspandy/LinearActivity1.java
@@ -0,0 +1,14 @@
+package com.example.project1_rendyhafizguspandy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LinearActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear1);
+    }
+}
